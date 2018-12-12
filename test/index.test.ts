f2260808d580a2c25514a2a004b2a15270a00964
Y0hy0h <Y0hy0h@users.noreply.github.com>
2018-12-12 19:12:39 +0100

    Add test for missing file

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,19 +1,11 @@
-import {expect, test} from '@oclif/test'
+import { expect, test } from '@oclif/test'
+import * as Errors from '../src/errors'
 
 import cmd = require('../src')
 
 describe('elmegram', () => {
   test
-    .stdout()
-    .do(() => cmd.run([]))
-    .it('runs hello', ctx => {
-      expect(ctx.stdout).to.contain('hello world')
-    })
-
-  test
-    .stdout()
-    .do(() => cmd.run(['--name', 'jeff']))
-    .it('runs hello --name jeff', ctx => {
-      expect(ctx.stdout).to.contain('hello jeff')
-    })
+    .do(() => cmd.run(['./nonexistant']))
+    .exit(Errors.FILE_NOT_FOUND)
+    .it('fails when src file cannot be found')
 })
